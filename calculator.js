61a5374a1ef604339a3de414e6db78266942efdd
Harsh5itha <45786603+Harsh5itha@users.noreply.github.com>
2021-10-17 01:04:31 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/index.html", function(req, res){
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1+num2;
